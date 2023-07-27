c52ad95b249b2e8b98c3b6a0e788b01b75d37ea2
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2023-07-27 11:45:31 +0700

    update

--- POP client Email.py
+++ POP client Email.py
@@ -1,16 +1,16 @@
 import poplib
 from email.message import EmailMessage
 
-server = "127.0.0.1"
-username = "inet"
-password = "1234"
+server = "192.168.15.129"
+user = "inet"
+passwd = "1234"
 
 server = poplib.POP3_SSL(server)
-server.user(username)
-server.pass_(password)
+server.user(user)
+server.pass_(passwd)
 
 msgNum = len(server.list()[1])
 
 for i in range(msgNum):
     for msg in server.retr(i+1)[1]:
-        print(msg)
+        print(msg.decode())
