c18d55d0a6321100e0d0fe5cbd1849b223c2e62f
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2023-08-17 11:23:36 +0700

    update

---
+++ W7/Chat-App/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const socket = require('socket.io');
+const io = socket(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected...');
+
+    socket.on('disconnect', (socket) => {  //
+        console.log('user disconnected...'); //
+    }); //
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
