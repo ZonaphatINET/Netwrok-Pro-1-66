24eb3015484ff3e67eaf573a7777912dbc141403
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2023-09-14 12:20:30 +0700

    update

---
+++ W10-11/test.py
@@ -0,0 +1,23 @@
+import telnetlib
+
+username='cisco'
+password='cisco'
+IP='192.168.1.116'
+
+tn = telnetlib.Telnet(IP)
+
+tn.read_until(b"Username: ")
+tn.write(username.encode('ascii') + b"\n")
+
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"enable\n")
+tn.write(b"cisco\n")
+tn.write(b"conf t\n")
+tn.write(b"int lo0\n")
+tn.write(b"ip add 10.10.10.10 255.255.255.255 \n")
+tn.write(b"end\n")
+tn.write(b"exit\n")
+print(tn.read_all().decode('ascii'))
