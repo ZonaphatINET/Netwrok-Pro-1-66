9665f2c1a36fce57ab03516c2ec7a91860687d6d
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2023-09-21 12:36:27 +0700

    update

--- W10-11/test.py
+++ W10-11/test.py
@@ -13,11 +13,10 @@ if password:
     tn.read_until(b"Password: ")
     tn.write(password.encode('ascii') + b"\n")
 
-tn.write(b"enable\n")
-tn.write(b"cisco\n")
 tn.write(b"conf t\n")
-tn.write(b"int lo0\n")
+tn.write(b"int lo 10\n")
 tn.write(b"ip add 10.10.10.10 255.255.255.255 \n")
+tn.write(b"no shutdown\n")
 tn.write(b"end\n")
 tn.write(b"exit\n")
 print(tn.read_all().decode('ascii'))
